4648917cbe24673b643904eadf260aab2f0deb15
Tom Nguyen <27g_2@amstedrail.com>
2023-12-07 14:12:44 +0700

    chapter 2 - 18 - Equality in Hibernate

--- Chapter 2/Demo/bookingwebapp/src/main/java/com/utradi/bookingwebapp/domain/Author.java
+++ Chapter 2/Demo/bookingwebapp/src/main/java/com/utradi/bookingwebapp/domain/Author.java
@@ -2,6 +2,7 @@ package com.utradi.bookingwebapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -45,5 +46,28 @@ public class Author {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author author)) return false;
+
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
