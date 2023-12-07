fc27482dfdbf06eb5d7a1f778700aa4b6c714eb4
Tom Nguyen <27g_2@amstedrail.com>
2023-12-07 12:25:56 +0700

    Add project files.

---
+++ Chapter 2/Demo/bookingwebapp/src/main/java/com/utradi/bookingwebapp/BookingwebappApplication.java
@@ -0,0 +1,13 @@
+package com.utradi.bookingwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingwebappApplication.class, args);
+	}
+
+}
